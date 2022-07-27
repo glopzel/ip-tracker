f2dda0a9ede9c92bbee0eef5e262b5ea67ce146f
glopzel <74939915+lozzel@users.noreply.github.com>
2022-07-27 19:38:39 -0400

    deleted commented code + added actual comments

--- js/main.js
+++ js/main.js
@@ -11,23 +11,21 @@ const displayISP = document.querySelector('#isp');
 btn.addEventListener('click', fetchIP)
 
 async function fetchIP() {
+    // if you get the value outside of this function on a global scope (at the top, right away on page load), you will get undefined, not the value
     let ipTest = inputIP.value
-    // var ipTest = '8.8.8.8';
-    // let ipTest = '192.212.174.101'
-    // var api_key = 'at_A1VUU3MAyiYPp0YA4IuDM9MCGVclA';
-    // var api_url = 'https://geo.ipify.org/api/v1?';
-    // var url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip;
+
     let connectionUrl = `https://geo.ipify.org/api/v1?apiKey=at_A1VUU3MAyiYPp0YA4IuDM9MCGVclA&ipAddress=${ipTest}`
     
     const results = await fetch(connectionUrl);
     const data = await results.json()
-    console.log(data)
 
+    // info to display
     let ip = data.ip;
     let location = `${data.location.city}, ${data.location.region}, ${data.location.postalCode}`
     let timezone = data.location.timezone
     let isp = data.isp
 
+    // coordinates for the map
     let latlng = L.latLng(data.location.lat, data.location.lng);
     mapIt(latlng)
 
@@ -35,9 +33,6 @@ async function fetchIP() {
 }
 
 function mapIt(ltlg) {
-    // `https://geo.ipify.org/api/v2/country?apiKey=at_A1VUU3MAyiYPp0YA4IuDM9MCGVclA&ipAddress=${ip}`
-    // old link https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-    // let map = L.map('map').setView([51.505, -0.09], 13);
     let map = L.map('map').setView(ltlg, 13)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
